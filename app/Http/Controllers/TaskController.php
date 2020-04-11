15fe836234a628b45ada9c1735455fc7deb54259
hirokichi1992 <plastar14105@gmail.com>
2020-04-11 17:59:49 +0900

    タスク編集機能追加

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Folder;
 use App\Http\Requests\CreateTask;
+use App\Http\Requests\EditTask;
 use App\Task;
 use Illuminate\Http\Request;
 
@@ -58,6 +59,21 @@ class TaskController extends Controller
 
         return redirect()->route('tasks.index', [
             'id' => $current_folder->id,
+        ]);
+    }
+
+    public function edit(int $id, int $task_id, EditTask $request)
+    {
+
+        $task = Task::find($task_id);
+
+        $task->title = $request->title;
+        $task->status = $request->status;
+        $task->due_date = $request->due_date;
+        $task->save();
+
+        return redirect()->route('tasks.index', [
+            'id' => $task->folder_id,
         ]);
 
     }
