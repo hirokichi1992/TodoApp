03dd7142e9380bb72ec0f94db1d96f21b45fae79
hirokichi1992 <plastar14105@gmail.com>
2020-04-24 14:13:19 +0900

    seedファイル修正

--- database/seeds/TasksTableSeeder.php
+++ database/seeds/TasksTableSeeder.php
@@ -11,15 +11,17 @@ class TasksTableSeeder extends Seeder
      */
     public function run()
     {
-        foreach (range(1,3) as $num){
-            DB::table('tasks') -> insert([
-                'folder_id' => 1,
-                'title' => "サンプルタスク{$num}",
-                'status' => $num,
-                'due_date' => \Carbon\Carbon::now()->addDay($num),
-                'created_at' => \Carbon\Carbon::now(),
-                'updated_at' => \Carbon\Carbon::now()
-            ]);
+        foreach (range(1, 3) as $folder_id) {
+            foreach (range(1, 3) as $num) {
+                DB::table('tasks')->insert([
+                    'folder_id' => $folder_id,
+                    'title' => "サンプルタスク{$num}",
+                    'status' => $num,
+                    'due_date' => \Carbon\Carbon::now()->addDay($num),
+                    'created_at' => \Carbon\Carbon::now(),
+                    'updated_at' => \Carbon\Carbon::now()
+                ]);
+            }
         }
     }
 }
